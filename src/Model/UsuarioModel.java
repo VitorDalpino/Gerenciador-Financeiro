f88b9308145677700530e6a63a087eb1ab4627ef
matheus correa <math_10correa@hotmail.com>
2025-03-31 20:44:29 -0300

    criacao models

---
+++ src/Model/UsuarioModel.java
@@ -0,0 +1,52 @@
+package Model;
+
+public class UsuarioModel {
+    public int id;
+    public String nome;
+    public String login;
+    public String senha;
+
+    // Construtor vazio
+    public UsuarioModel() {
+    }
+
+    // Construtor com parâmetros
+    public UsuarioModel(int id, String nome, String login, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
